ccfe6ff714e226ef95ce3efe833d4082e062ee1d
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-08 16:09:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@
 // you can also load the code snippets in using your browser's console
 const main = document.getElementById('main')
 main.addEventListener('click', function (event){
-  alert('I was clicked')
+  alert('I was clicked!')
 }); 
