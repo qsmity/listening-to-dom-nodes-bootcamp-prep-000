a276044b42c43dbd7f22c3466e9bea4f750b8035
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-08 16:42:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -43,3 +43,15 @@ for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', capture, true);
 }
 
+const divs = document.querySelectorAll('div');
+ 
+function bubble(e) {
+  // stop! that! propagation!
+  e.stopPropagation();
+ 
+  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+}
+ 
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', bubble);
+}
